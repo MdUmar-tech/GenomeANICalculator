9122f95aca2641339f396c55e1a43bb5f742d938
MD UMAR <76789751+MdUmar-tech@users.noreply.github.com>
2023-12-17 12:00:35 +0530

    Update remove_spaces.py

--- remove_spaces.py
+++ remove_spaces.py
@@ -7,7 +7,16 @@ def replace_spaces_with_underscores(directory):
             old_path = os.path.join(directory, filename)
             new_path = os.path.join(directory, new_filename)
             os.rename(old_path, new_path)
-            print(f'Renamed: {old_path} to {new_path}')
+            print(f'Renamed: {filename} to {new_filename}')
 
-# Replace 'your_directory_path' with the path of the directory containing your files
-replace_spaces_with_underscores('genomes')
+def main():
+    folder_name = input("Enter the folder name: ")
+    directory_path = os.path.join(os.getcwd(), folder_name)
+
+    if os.path.exists(directory_path) and os.path.isdir(directory_path):
+        replace_spaces_with_underscores(directory_path)
+    else:
+        print(f"The specified folder '{folder_name}' does not exist or is not a directory.")
+
+if __name__ == "__main__":
+    main()
